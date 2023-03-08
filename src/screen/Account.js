dcaaa67a313ec86ab3825469321d8052d4cfc6bf
“Sebastian <iscrozo@gmail.com>
2023-03-07 22:32:38 -0500

    init the proyect with navigation and build screens

---
+++ src/screen/Account.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Account() {
+  return (
+    <SafeAreaView>
+      <Text>Account</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Account;
---
+++ src/screen/Pokedex.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Pokedex() {
+  return (
+    <SafeAreaView>
+      <Text>Pokedex</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Pokedex;
