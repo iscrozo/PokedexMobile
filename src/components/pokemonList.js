6138872015304309fe0962cfb5457c77a9d802fb
“Sebastian <iscrozo@gmail.com>
2023-03-14 09:18:13 -0500

    add scroll in flatList

--- src/components/pokemonList.js
+++ src/components/pokemonList.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { StyleSheet, View, Text, FlatList } from "react-native";
+import { StyleSheet, FlatList, ActivityIndicator } from "react-native";
 import PokemonCard from "./PokemonCard";
 
 function pokemonList(props) {
   //console.log(props);
   const { pokemonData } = props;
   //(pokemonData);
+
+  const loadMore = () => {
+    console.log("Cargando mas pokemons...");
+  };
   return (
     <FlatList
       data={pokemonData}
@@ -14,6 +18,15 @@ function pokemonList(props) {
       keyExtractor={(pokemon) => String(pokemon.id)}
       renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
+      onEndReached={loadMore}
+      onEndReachedThreshold={0.1}
+      ListFooterComponent={
+        <ActivityIndicator
+          size="large"
+          style={styles.spinner}
+          color="#AEAEAE"
+        />
+      }
     />
   );
 }
@@ -23,5 +36,9 @@ export default pokemonList;
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
+  },
+  spinner: {
+    marginTop: 20,
+    marginBottom: 60,
   },
 });
